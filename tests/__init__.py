b6530c6479f73024f04e3a0613d3189c0ff759c5
Ryan Lane <rlane@lyft.com>
2016-08-29 16:38:35 -0700

    Add test info

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+# Make the tests directory a python module
+# so that all unit tests are reported as part of the tests package.
+
+import os
+
+# Inject mandatory environment variables
+env_settings = [
+    ('AWS_DEFAULT_REGION', 'us-east-1'),
+    ('DEBUG', 'true')
+]
+
+for env_setting in env_settings:
+    os.environ[env_setting[0]] = os.getenv(env_setting[0], env_setting[1])

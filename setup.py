21e07c9d72f61546cff488eb2db12673dea22e8b
Dean Liu <dliu@lyft.com>
2022-04-11 21:14:47 -0700

    merge (#41)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="confidant-client",
-    version="2.2.2",
+    version="2.2.3",
     packages=find_packages(exclude=["test*"]),
     install_requires=[
         # Boto3 is the Amazon Web Services (AWS) Software Development Kit (SDK)

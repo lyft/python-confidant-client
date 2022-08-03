490515cd195225d79f71ae5227cbcae252551068
Erick Durán <eduran@lyft.com>
2022-08-03 15:21:14 -0700

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="confidant-client",
-    version="2.2.5",
+    version="2.2.6",
     packages=find_packages(exclude=["test*"]),
     install_requires=[
         # Boto3 is the Amazon Web Services (AWS) Software Development Kit (SDK)

